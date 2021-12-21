e656a0701c76dc4065df02da6d78787730053260
Kevo <me@kevo.link>
2021-12-21 17:29:24 +0100

    Fix tests

--- tests/test_get_comments.py
+++ tests/test_get_comments.py
@@ -2,5 +2,11 @@ from invidious_api_client import InvidiousAPIClient
 
 
 
-CLIENT = InvidiousAPIClient()
-# TODO
+def test_comments():
+    CLIENT = InvidiousAPIClient()
+    # TODO
+
+
+
+if __name__ == "__main__":
+    test_comments()
--- tests/test_instances.py
+++ tests/test_instances.py
@@ -2,5 +2,11 @@ from invidious_api_client import get_instances
 
 
 
-for instance in get_instances().instances:
-    print(f"{instance.uri} ({instance.region}, {f'{instance.monitor.month_ratio.ratio} % uptime in the last month' if instance.monitor else '[no monitor data]'}")
+def test_instances():
+    for instance in get_instances().instances:
+        print(f"{instance.uri} ({instance.region}, {f'{instance.monitor.month_ratio.ratio} % uptime in the last month' if instance.monitor else '[no monitor data]'}")
+
+
+
+if __name__ == "__main__":
+    test_instances()
